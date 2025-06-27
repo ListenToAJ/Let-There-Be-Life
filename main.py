1d6b4d57246f607be80000aac5123ce80fe1a540
AJ Simao <simaospamm@gmail.com>
2025-06-27 14:56:10 -0400

    Got first working prototype done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pygame
+import time
 
 # CONSTANTS
 GRID_SIZE = (50, 50)
@@ -18,7 +19,7 @@ def setup_display():
 
 def draw_grid(grid: np.array, screen: pygame.display):
     """
-    Pass 2d numpy array and draw it to screen (boolean values)
+    Pass 2d numpy array and draw it to screen (boolean values).
     """
     for y,x in np.ndindex(grid.shape):
         # Get color for pixel value
@@ -32,15 +33,61 @@ def draw_grid(grid: np.array, screen: pygame.display):
     pygame.display.flip()
 
 def update_grid(grid: np.array, click_pos: tuple, button: int):
+    """
+    Take click pos and button click and modify grid.
+    """
     # Convert click pos coords to grid space coords
     grid_space = tuple(x // PIXEL_SIZE for x in click_pos)
 
+    # Click conditional
     if button == 1:
         grid[grid_space] = True
     elif button == 3:
         grid[grid_space] = False
     else:
         raise ValueError("How tf did we get here brother?")
+    
+def step(grid: np.array) -> np.array:
+    """
+    Create next grid frame with Conways rules
+    """
+    next_step = grid.copy()
+
+    cell_neighbors = [
+        (-1, -1), (-1, 0), (-1, +1),
+        ( 0, -1),          ( 0, +1),
+        (+1, -1), (+1, 0), (+1, +1)
+    ]
+
+    # Loop through initial state cells
+    for y, x in np.ndindex(grid.shape):
+        neighbor_count = 0
+
+        for dy, dx in cell_neighbors:
+            # Get neighbor coordinates (With wraparound from modulo)
+            ny = (y + dy) % grid.shape[0]
+            nx = (x + dx) % grid.shape[1]
+            
+            # Accumulate neighbor counts
+            if grid[(ny, nx)] == True:
+                neighbor_count += 1
+
+        if grid[(y, x)] == True:
+            if neighbor_count < 2:
+                # Underpopulation
+                next_step[(y, x)] = False
+            elif neighbor_count == 2 or neighbor_count == 3:
+                # Survival
+                next_step[(y, x)] = True
+            elif neighbor_count > 3:
+                # Overpopulation
+                next_step[(y,x)] = False
+        else:
+            if neighbor_count == 3:
+                # Reproduction
+                next_step[(y,x)] = True
+
+    return next_step
 
 if __name__ == '__main__':
     # Setup screen size    
@@ -51,6 +98,7 @@ if __name__ == '__main__':
 
     # Main loop
     running = True
+    playing = False
     dragging_left = False
     dragging_right = False
 
@@ -58,6 +106,7 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
             # Mouse down event
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
@@ -66,12 +115,14 @@ if __name__ == '__main__':
                 if event.button == 3:
                     dragging_right = True
                     update_grid(grid=grid, click_pos=tuple(reversed(event.pos)), button=event.button)
+
             # Mouse up event
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     dragging_left = False
                 if event.button == 3:
                     dragging_right = False
+
             # Mouse drag event
             elif event.type == pygame.MOUSEMOTION:
                 if dragging_left:
@@ -79,7 +130,17 @@ if __name__ == '__main__':
                 if dragging_right:
                     update_grid(grid=grid, click_pos=tuple(reversed(event.pos)), button=3)
 
-            draw_grid(grid=grid, screen=screen)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    playing ^= True
+                    print(f"Playing bool is set to: {playing}")
 
+                if event.key == pygame.K_RIGHT:
+                    grid = step(grid=grid)
 
+        if playing:
+            grid = step(grid=grid)
+            time.sleep(0.2)
+
+        draw_grid(grid=grid, screen=screen)
         
